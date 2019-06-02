b4a32d58cc3813a3f9eaacc3f11aba57902c4bc3
tim <tim@TIMSPC>
2019-06-02 23:30:38 +0300

    Added level map

---
+++ level/VoltageTileSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="VoltageTileSet" tilewidth="30" tileheight="30" tilecount="64" columns="8">
+ <image source="Tiles.png" width="240" height="240"/>
+</tileset>
